9222d8c27c66a55667a52ac667e4b4bfdc11dabf
xeedis <k.tarnowski99@gmail.com>
2023-10-26 21:50:16 +0200

    first commit

---
+++ CalculatorOperations.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Calculator;
+public class CalculatorOperations
+{
+    public static double CalculateResult(string operation)
+    {
+        var fmt = new NumberFormatInfo();
+        fmt.NegativeSign = "-";
+        if (operation.Contains("+"))
+        {
+            var addElements = operation.Split('+');
+
+            return double.Parse(addElements[0],fmt) + double.Parse(addElements[1],fmt);
+        }
+        if (operation.Contains("-"))
+        {
+            var addElements = operation.Split('-');
+
+            return double.Parse(addElements[0]) - double.Parse(addElements[1]);
+        }
+        if (operation.Contains("/"))
+        {
+            var addElements = operation.Split('/');
+
+            return double.Parse(addElements[0]) / double.Parse(addElements[1]);
+        }
+        if (operation.Contains("*"))
+        {
+            var addElements = operation.Split('*');
+
+            return double.Parse(addElements[0]) * double.Parse(addElements[1]);
+        }
+        return default;
+
+    }
+
+    public static bool ConatinsOperation(string operation)
+    {
+        return operation.Contains('+') || operation.Contains("-") || operation.Contains('*') || operation.Contains("/"); 
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        ResultText.Text = string.Empty;
+    }
+
+    private bool isNegative = false;
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+
+        var currentNumber = button?.Name[button.Name.Length - 1];
+        CurrentOperationText.Text += currentNumber;
+    }
+
+    private void ClearButton_Click(object sender, RoutedEventArgs e)
+    {
+        ResultText.Text = string.Empty;
+        CurrentOperationText.Text = string.Empty;
+    }
+
+    private void ChangeValueButton_Click(object sender, RoutedEventArgs e)
+    {
+        /*isNegative = !isNegative;
+        if (isNegative)
+        {
+            CurrentOperationText.Text = "-" + CurrentOperationText.Text;
+        }
+        else
+        {
+            if (CurrentOperationText.Text.Length > 0 && CurrentOperationText.Text[0] == '-')
+            {
+                CurrentOperationText.Text = CurrentOperationText.Text.Substring(1);
+            }
+        }*/
+    }
+
+    private void OperatorButton_Click(object sender, RoutedEventArgs e)
+    {
+        var button = sender as Button;
+
+        var currentOperation = button?.Content.ToString();
+
+        CurrentOperationText.Text += currentOperation;
+        isNegative = false; 
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        var operation = CurrentOperationText.Text;
+        if(CalculatorOperations.ConatinsOperation(operation))
+        {
+            CurrentOperationText.Text = CalculatorOperations.CalculateResult(operation).ToString();
+        }
+
+        CurrentOperationText.Text += "+";
+    }
+
+    private void MinusButton_Click(object sender, RoutedEventArgs e)
+    {
+        var operation = CurrentOperationText.Text;
+        if (CalculatorOperations.ConatinsOperation(operation))
+        {
+            CurrentOperationText.Text = CalculatorOperations.CalculateResult(operation).ToString();
+        }
+
+        CurrentOperationText.Text += "-";
+    }
+
+    private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+    {
+        var operation = CurrentOperationText.Text;
+        if (CalculatorOperations.ConatinsOperation(operation))
+        {
+            CurrentOperationText.Text = CalculatorOperations.CalculateResult(operation).ToString();
+        }
+
+        CurrentOperationText.Text += "*";
+    }
+
+    private void DivideButton_Click(object sender, RoutedEventArgs e)
+    {
+        var operation = CurrentOperationText.Text;
+        if (CalculatorOperations.ConatinsOperation(operation))
+        {
+            CurrentOperationText.Text = CalculatorOperations.CalculateResult(operation).ToString();
+        }
+
+        CurrentOperationText.Text += "/";
+    }
+
+    private void ResultButton_Click(object sender, RoutedEventArgs e)
+    {
+        var operation = CurrentOperationText.Text;
+
+        var result = CalculatorOperations.CalculateResult(operation);
+
+        ResultText.Text = result.ToString();
+
+        CurrentOperationText.Text = string.Empty;
+    }
+}
